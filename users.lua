c1978710d9e73d68ec542380f13b64e434f1c731
Your Name <you@example.com>
2023-10-08 22:34:22 +0700

    add check and sort

--- users.lua
+++ users.lua
@@ -55,140 +55,141 @@ end
 log = print
 
 client:on('messageCreate', function(message)
-if message.content == '!checkmembers' then
-	print("catch checkmembers")
-	RoleForPing_id = ""
-	RoleForCheck_id = ""
-	if message.guild == nil then
-		message:addReaction("✅");
-		return;
-	end
-
-	local roles = message.guild.roles
+	if message.content == '!checkmembers' then
+		log("Catch checkmembers:" .. message.author.name)
 	
-	for _, role in pairs(roles) do
-		if role.name == RoleForCheck then
-			print("Find role ".. RoleForCheck .. " " .. role.id)
-			RoleForCheck_id = role.id
+		RoleForPing_id = ""
+		RoleForCheck_id = ""
+		if message.guild == nil then
+			message:addReaction("✅");
+			return;
 		end
-	end
-
-	if RoleForCheck_id == nil then
-		log("Not found role: " .. RoleForCheck);
-		return;
-	end
-
-	for _, role in pairs(roles) do
-		if role.name == RoleForPing then
-			print("Find role ".. RoleForPing .. " " .. role.id)
-			RoleForPing_id = role.id
-		end
-	end
-
-	if RoleForPing_id == nil then
-		log("Not found role: " .. RoleForPing);
-		return;
-	end
-
-	local guild = message.guild
-	local textChannels = guild.textChannels
-
-	ChannelForPing_id = ""
-	for _, channel in pairs(textChannels) do
-		if channel.name == ChannelForPing then
-			ChannelForPing_id = channel.id
-			print("Find channel " .. ChannelForPing .. " " .. channel.id)
-			if message.channel.id == channel.id then
-				message:addReaction("✅");
-			end
-
-		end
-	end
 	
-	local role = guild.roles:get(RoleForCheck_id)
-	local membersWithRole = {}
-
-	local allMembers = guild.members:toArray()
-	for _, guildMember in ipairs(allMembers) do
-		if guildMember:hasRole(role) then
-			table.insert(membersWithRole, guildMember)
-			if guildMember.nickname == nil then
-				log("No nickname for: " .. guildMember.name );
-			end
-
-		end
- 		
-	end
-	
-	saveMembers(membersWithRole)
-	
-	inGame = importNames()
-	if importNames == nil then
-		log("ERROR");
-	end
-	
-	NotFoundMembers = {}
-	for _, GameNick in ipairs(inGame) do
-		--print(GameNick);
-		local found = false
-
-		for _, DiscordNick in ipairs(membersWithRole) do
-			if string.find(string.lower(DiscordNick.name), string.lower(GameNick)) then
-				-- print("Find member: " .. GameNick); 
-				found = true
-				break;
+		local guild = message.guild
+		local roles = guild.roles
+		
+		for _, role in pairs(roles) do
+			if role.name == RoleForCheck then
+				print("Find role ".. RoleForCheck .. " " .. role.id)
+				RoleForCheck_id = role.id
 			end
 		end
-		if found == false then
-			print("Dont find member in discrod: " .. GameNick)
-			table.insert(NotFoundMembers, GameNick);
+	
+		if RoleForCheck_id == nil then
+			log("Not found role: " .. RoleForCheck);
+			return;
 		end
-	end
-
-	answer = "Не удалось найти в discord:\n"
-	for _, Nick in ipairs(NotFoundMembers) do
-		answer = answer .. Nick .. "\n"
-	end
-
-	message:reply("" .. answer)
-
-	answer = ""
-	for _, DiscordNick in ipairs(membersWithRole) do
-		print(DiscordNick.name);
-		local found = false
-		local SP, EP = string.find(DiscordNick.name, "]") -- remove ]
-		local EditedNickname = ""
-		if SP ~= nil then
-			EditedNickname = string.sub(DiscordNick.name, SP+1) -- find word after ]
-			print(EditedNickname)
-			EditedNickname = string.match(EditedNickname or DiscordNick.name, "%w+")
-			local EditedNickname_low = string.lower(EditedNickname or DiscordNick.name)
-			for _, GameNick in ipairs(inGame) do
-				print(string.lower(GameNick).." and "..EditedNickname_low);
-				if string.find(string.lower(GameNick), EditedNickname_low) then
+	
+		for _, role in pairs(roles) do
+			if role.name == RoleForPing then
+				print("Find role ".. RoleForPing .. " " .. role.id)
+				RoleForPing_id = role.id
+			end
+		end
+	
+		if RoleForPing_id == nil then
+			log("Not found role: " .. RoleForPing);
+			return;
+		end
+	
+		local textChannels = guild.textChannels
+	
+		ChannelForPing_id = ""
+		for _, channel in pairs(textChannels) do
+			if channel.name == ChannelForPing then
+				ChannelForPing_id = channel.id
+				print("Find channel " .. ChannelForPing .. " " .. channel.id)
+				if message.channel.id == channel.id then
+					message:addReaction("✅");
+				end
+	
+			end
+		end
+		
+		local role = guild.roles:get(RoleForCheck_id)
+		local membersWithRole = {}
+	
+		local allMembers = guild.members:toArray()
+		for _, guildMember in ipairs(allMembers) do
+			if guildMember:hasRole(role) then
+				table.insert(membersWithRole, guildMember)
+				if guildMember.nickname == nil then
+					log("No nickname for: " .. guildMember.name );
+				end
+	
+			end
+	 		
+		end
+		
+		saveMembers(membersWithRole)
+		
+		inGame = importNames()
+		if importNames == nil then
+			log("ERROR");
+		end
+		
+		NotFoundMembers = {}
+		for _, GameNick in ipairs(inGame) do
+			--print(GameNick);
+			local found = false
+	
+			for _, DiscordNick in ipairs(membersWithRole) do
+				if string.find(string.lower(DiscordNick.name), string.lower(GameNick)) then
 					-- print("Find member: " .. GameNick); 
 					found = true
 					break;
 				end
 			end
 			if found == false then
-				print("Dont find member in game: " .. DiscordNick.name)
-				answer = answer .. DiscordNick.name .. "\n"
-				message:reply("Не удалось найти в игре: " .. DiscordNick.name .. "\n")
+				print("Dont find member in discrod: " .. GameNick)
+				table.insert(NotFoundMembers, GameNick);
 			end
 		end
-	end
-
-
-	--[[
-	REWRITEE!!!!!!!
 	
-	I DONT WANT SEEE CODE LIKE THIS
-
-	]]--
-
-
-end
+		answer = "Не удалось найти в discord:\n"
+		table.sort(NotFoundMembers)
+		for _, Nick in ipairs(NotFoundMembers) do
+			answer = answer .. Nick .. "\n"
+		end
+	
+		message:reply("" .. answer)
+	
+		answer = ""
+		for _, DiscordNick in ipairs(membersWithRole) do
+			print(DiscordNick.name);
+			local found = false
+			local SP, EP = string.find(DiscordNick.name, "]") -- remove ]
+			local EditedNickname = ""
+			if SP ~= nil then
+				EditedNickname = string.sub(DiscordNick.name, SP+1) -- find word after ]
+				print(EditedNickname)
+				EditedNickname = string.match(EditedNickname or DiscordNick.name, "%w+")
+				local EditedNickname_low = string.lower(EditedNickname or DiscordNick.name)
+				for _, GameNick in ipairs(inGame) do
+					print(string.lower(GameNick).." and "..EditedNickname_low);
+					if string.find(string.lower(GameNick), EditedNickname_low) then
+						-- print("Find member: " .. GameNick); 
+						found = true
+						break;
+					end
+				end
+				if found == false then
+					answer = answer .. DiscordNick.name .. "\n"
+					--message:reply("Не удалось найти в игре: " .. DiscordNick.name .. "\n")
+				end
+			end
+		end
+		message:reply("Не удалось найти в игре: \n" .. answer .. "\n")
+	
+		--[[
+		REWRITEE!!!!!!!
+		
+		I DONT WANT SEEE CODE LIKE THIS
+	
+		]]--
+	
+	
+	end
 end)
 
 client:run("Bot " .. token)
