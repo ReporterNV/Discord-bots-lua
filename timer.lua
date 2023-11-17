9385b0733bc340b39e15ebcff9ef4617e47673da
Your Name <you@example.com>
2023-11-18 05:42:48 +0700

    rewrite this. Finaly something works!

--- timer.lua
+++ timer.lua
@@ -1,51 +1,70 @@
 local token = require("token")
 local discordia = require("discordia")
+local timer = require('timer')
+
 local client = discordia.Client()
-
-local function sleep(seconds)
-  os.execute("sleep " .. tonumber(seconds))
+local function threadFunc(message, note)
+	--print(value)
+	--print(note)
+	message:reply(message.author.mentionString .. note)
 end
 
-local function threadFunc(message, value, str)
-	print(value)
-	print(str)
-  message:reply(message.author.mentionString .." Начало потока")
-  sleep(value)
-  message:reply("Таймер завершен! " .. str)
-end
-
---[[
-local str = "!timer 20h44m33s msg"
-
-local timer = string.match(str, "[%s]%w+%s")
-print("\""..timer.."\"")
-
-local hours = string.match(timer, "%d+[h]")
-local min = string.match(timer, "%d+[m]")
-local sec = string.match(timer, "%d+[s]")
-
-print(hours)
-print(min)
-print(sec)
---]]
 
 client:on("messageCreate", function(message)
 	if message.author.bot then return end
 	local content = message.content
 
 	if string.sub(content, 1, 6) == "!timer" then
-		print(string.sub(content, 8, 9))
-		if tonumber(string.sub(content, 8, 9)) then
-			local thread = coroutine.create(threadFunc)
-			coroutine.resume(
-			    thread, 
-			    message,
-			    tonumber(string.sub(content, 8, 9)),
-			    string.sub(content, 10)
-			)
-		else
-			client:stop()
-		end
+		print(content)
+		local time_str = string.match(content, "%s(%w+)%s")
+		print("time: \""..time_str.."\"")
+		local _, endtimer = content:find(time_str)
+		local note = content:sub(endtimer+2)
+		print("note: \""..note.."\"")
+
+		--[[
+		local years = string.match(timer, "%d+[y]")
+		local month = string.match(timer, "%d+[M]")
+		--]]
+		local weeks = (string.match(time_str, "(%d+)[d]") or 0)
+		local days  = (string.match(time_str, "(%d+)[d]") or 0)
+		local hours = (string.match(time_str, "(%d+)[h]") or 0)
+		local min = (string.match(time_str, "(%d+)[m]") or 0)
+		local sec = (string.match(time_str, "(%d+)[s]") or 0)
+		--[[
+		print(years)
+		print(month)
+		--]]
+		print("weeks: "..weeks)
+		print("days: "..days)
+		print("hours: "..hours)
+		print("min: "..min)
+		print("sec: "..sec)	
+		
+
+		time = sec +
+			min*60 +
+			hours*60*60 +
+			days*24*60*60 +
+			weeks*7*24*60*60
+
+		print("sec to timer: " .. time)
+		message:reply("Таймер установлен")
+		print(message)
+		print(time)
+		print(note)
+		local flag = 0
+		timer.setTimeout(time * 1000, coroutine.wrap(threadFunc), message, note )
+
+		--[[
+		local thread = coroutine.create(threadFunc)
+		coroutine.resume(
+		    thread, 
+		    message,
+		    timer,
+		    note
+		)
+		-]]
 	end
 end)
 
