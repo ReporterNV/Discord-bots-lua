06823988b18fa358acd9c9ed68626d48dad9b914
Your Name <you@example.com>
2024-06-13 05:07:37 +0700

    change calc len before note & change log msg

--- bots/timer.lua
+++ bots/timer.lua
@@ -18,37 +18,45 @@ client:on("messageCreate", function(message)
 		local time_str = string.match(content, "%s(%w+)%s*");
 		print("time: \""..time_str.."\"");
 		local _, endtimer = content:find(time_str); --rewrite it with use #
-		local note = content:sub(endtimer+2);
+		--local note = content:sub(endtimer+2);
+		local note = content:sub(#"!timer" + #time_str);
 		print("note: \""..note.."\"");
-		--[[
-		local years = string.match(timer, "%d+[y]")
-		local month = string.match(timer, "%d+[M]")
-		--]]
-		local weeks = (string.match(time_str, "(%d+)[w]") or 0);
-		local days  = (string.match(time_str, "(%d+)[d]") or 0);
-		local hours = (string.match(time_str, "(%d+)[h]") or 0);
-		local min = (string.match(time_str, "(%d+)[m]") or 0);
-		local sec = (string.match(time_str, "(%d+)[s]") or 0);
-		--[[
-		print(years)
-		print(month)
-		--]]
-		print("weeks: "..weeks .. 
-		      "days: "..days);
-		print("hours: "..hours);
-		print("min: "..min);
-		print("sec: "..sec);
-		
-		time = sec +
+
+		IsSeconds = tonumber(time_str); --check if not symbol for time like s,m,h,d or w;
+
+		if (IsSeconds) then
+			time = IsSeconds;
+		else
+			--[[
+			local years = string.match(timer, "%d+[y]")
+			local month = string.match(timer, "%d+[M]")
+			--]]
+			local weeks = (string.match(time_str, "(%d+)[w]") or 0);
+			local days  = (string.match(time_str, "(%d+)[d]") or 0);
+			local hours = (string.match(time_str, "(%d+)[h]") or 0);
+			local min = (string.match(time_str, "(%d+)[m]") or 0);
+			local sec = (string.match(time_str, "(%d+)[s]") or 0);
+			--[[
+			print(years);
+			print(month);
+			--]]
+			print(
+			"weeks: " .. weeks .. "; " ..
+			"days: " .. days .. "; " ..
+			"hours: " .. hours .. "; " ..
+			"min: " .. min .. "; " ..
+			"sec: " .. sec .. "; "
+			);
+			time = sec +
 			min*60 +
 			hours*60*60 +
 			days*24*60*60 +
 			weeks*7*24*60*60
-
+		end
 		print("timer in sec: " .. time);
 
 		message:addReaction("✅");
-		timer.setTimeout(time * 1000, coroutine.wrap(threadFunc), message, note);
+		timer.setTimeout(time * 1000, coroutine.wrap(threadFunc), message, note);--bcz message:reply works only in coroutine
 	end
 end)
 
